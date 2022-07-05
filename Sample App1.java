fb0be95e033f035ffac3b580305da91a80977c64
Jomari Joseph Barrera <107764850+infinityweaver@users.noreply.github.com>
2022-07-05 14:56:10 +0800

    Update Sample App1.java

--- Sample App1.java
+++ Sample App1.java
@@ -1,5 +1,8 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+
+        for (int i=1; i <= 100; i++)
+        	System.out.println(i+"");
     }
 }
