5ccac3ca4a901d071547b7aaf1322f77b9c4b43e
KaelNierras <kyleanthony47@gmail.com>
2022-07-05 14:33:15 +0800

    Update Sample App1.java

--- Sample App1.java
+++ Sample App1.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World! Yes"); 
-        System.out.println("Kyle Gwapo"); 
+        System.out.println("Kyle Gwapo kaayo"); 
     }
 }
