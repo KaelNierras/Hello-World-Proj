3fa5574a83ed6d4888dffd4bba9d3dbe654cf37a
Alvin Macion <macion.et.al@gmail.com>
2022-07-05 15:02:21 +0800

    Update Sample App1.java

--- Sample App1.java
+++ Sample App1.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        for (int i=0; i<=100; i++)
+        System.out.println("Parahubog");
     }
 }
